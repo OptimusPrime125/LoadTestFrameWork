cff59fb76665878adb0c62e5eaa264689083d233
OptimusPrime125 <transformerprime125@gmail.com>
2023-07-30 11:45:17 +0530

    CSV Fedder Code added

---
+++ src/test/scala/simulations/TestApiSimulationCSVFeeder.scala
@@ -0,0 +1,36 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+class TestApiSimulationCSVFeeder extends Simulation
+{
+  // Http Config
+  val HttpRequest: HttpProtocolBuilder = http.baseUrl("https://gorest.co.in")
+    .header("Accept", "application/json")
+    .header("Authorization", "Bearer 6e05c602e989c9c506686019302447de4f6986b5e4d6efa9ba26296994095d39")
+
+//CSV Feeder
+  val csvFeeder = csv("src/test/resources/Data/userdata.csv").circular
+  def getUser()=
+    {
+      repeat(3)
+      {
+        feed(csvFeeder)
+          .exec(http("GET A LIST OF USER").get("/public/v2/users/${userid}")
+            .check(status is 200)
+          )
+      }
+    }
+
+  //Scenario
+  val scn: ScenarioBuilder =scenario("GET DATA OF USER").exec(getUser())
+
+  //SetUp
+
+  setUp(scn.inject(atOnceUsers(1))).protocols(HttpRequest)
+
+}
---
+++ src/test/scala/simulations/TestApiSimulationGetRampUp.scala
@@ -0,0 +1,32 @@
+package simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.scenario.Simulation
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class TestApiSimulationGetRampUp extends Simulation {
+  // Http Config
+  val HttpRequest: HttpProtocolBuilder = http.baseUrl("https://reqres.in")
+    .header("Accept", "application/json")
+
+  //Scenario
+  val scn: ScenarioBuilder = scenario("First Project")
+    .exec(http("Get User Request")
+      .get("/api/users?page=2").check(status is 200)
+    )
+  //SetUp
+
+  setUp(
+    scn.inject
+    (nothingFor(5),
+      atOnceUsers(5),
+      rampUsers(10) during (10 seconds)
+    ).protocols(HttpRequest)
+  )
+
+}
